af49b4f2adfdd41883e61dfe5173c7c381d5043c
David Friday <davidsfriday13@gmail.com>
2019-03-08 15:54:39 -0600

    server set up?

--- sagas-portfolio-master/server/server.js
+++ sagas-portfolio-master/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-const plantRouter = require('./routes/profile.router');
+const profileRouter = require('./routes/profile.routes');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
